4a5e825205d93ea408de8d5043487cfc8438b38f
Michael Salerno <michael.j.salerno@stonybrook.edu>
2016-03-07 23:35:44 -0500

    can sniff now

--- mydump.c
+++ mydump.c
@@ -40,12 +40,22 @@ int main(int argc, char * argv[]) {
     }
 
     printf ("i = %s, r = %s, s = %s\n", ival, rval, sval);
-
     for (index = optind; index < argc; index++) {
         printf ("Non-option argument %s\n", argv[index]);
     }
 
-        return 0;
+    char errbuf[PCAP_ERRBUF_SIZE];
+    pcap_t *handle = NULL;
+
+    handle = pcap_open_live(ival, BUFSIZ, 1, 1000, errbuf);
+
+    if (handle == NULL) {
+        fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
+        return(2);
+    }
+
+
+    return 0;
 }
 
 void print_help(FILE *fd) {
@@ -53,7 +63,7 @@ void print_help(FILE *fd) {
     fprintf(fd, "-i  Listen on network device <interface> (e.g., eth0). If not specified, mydump\n");
     fprintf(fd, "    should select a default interface to listen on.\n\n");
     fprintf(fd, "-r  Read packets from <file> (tcpdump format).\n\n");
-    fprintf(fd, "-s  Keep only packets that contain <string> in their payload. You are not");
-    fprintf(fd, "    required to implement wildcard or regular expression matching. A simple");
-    fprintf(fd, "    string matching operation should suffice.");
+    fprintf(fd, "-s  Keep only packets that contain <string> in their payload. You are not\n");
+    fprintf(fd, "    required to implement wildcard or regular expression matching. A simple\n");
+    fprintf(fd, "    string matching operation should suffice.\n");
 }
